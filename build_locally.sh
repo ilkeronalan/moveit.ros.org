a4c3891b01cd881022c15cd2148b0470bb4ce73b
Dave Coleman <davetcoleman@gmail.com>
2019-04-24 19:44:44 -0600

    Improve build_locally.sh to have travis argument

--- build_locally.sh
+++ build_locally.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -eu
 # Locally build the Jekyl site for testing on Ubuntu 16.04
 
+have_travis() {
+  for arg in "$@"; do
+    if [[ "${arg}" == "travis" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 if [[ "$0" != "${BASH_SOURCE}" ]]; then
   {
     echo "This file is meant to be executed, not 'source'd:"
@@ -18,12 +27,22 @@ gem install jekyll
 gem install bundler -v 1.17.3
 bundle install
 
-# Build website using similar script as Travis, but skip htmlproofer because it fails locally:
-bundle exec jekyll build --strict_front_matter
+# Test website using same script as Travis
+if have_travis "$@"; then
+  ./.travis.sh
+fi
 
 # Launch website
-echo "Opening http://localhost:4000 in default browser"
-xdg-open http://localhost:4000
+echo
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo "Preparing to open http://localhost:4000"
+echo "Be sure to refresh auto-opened webpage"
+read -p "Press any key to start serving website..."
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo
+google-chrome http://localhost:4000
 
 # Start serving website locally
 bundle exec jekyll serve
